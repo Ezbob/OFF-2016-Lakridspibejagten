ee8a5aeec3ba896019ba0d71b78980763a7a55e4
Frederik Hertzum <frederik.hertzum@gmail.com>
2016-08-31 09:29:30 +0200

    main_map.h fikset

--- main_map.h
+++ main_map.h
@@ -1,7 +1,7 @@
 #ifndef UUID_8DB4C94D_C9A5_45BF_BDBA_FA8F202A4938
 #define UUID_8DB4C94D_C9A5_45BF_BDBA_FA8F202A4938
 
-#include <SFML/Texture/Texture.hpp>
+#include <SFML/Graphics/Texture.hpp>
 
 class MainMap : public sf::Texture {
 public:
