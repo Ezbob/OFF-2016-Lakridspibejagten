eac4ed7d6bc7f2c57b5388d0d989b73b80a1ac47
Frederik Hertzum <frederik.hertzum@gmail.com>
2016-09-01 00:30:35 +0200

    revert til fungerende sted

---
+++ src/assets.cpp
@@ -0,0 +1,11 @@
+#include "assets.hpp"
+
+namespace assets {
+	sf::Font font_main;
+	sf::Font font_description;
+
+	sf::Texture ball;
+	sf::Texture runner;
+	sf::Texture backgro9und;
+	sf::Texture world;
+}
