74cebb14c69493b3c74662a7a2aa6de4fd108815
Frederik Hertzum <frederik.hertzum@gmail.com>
2016-08-31 19:01:57 +0200

    paths.hpp

---
+++ src/paths.hpp
@@ -0,0 +1,13 @@
+#ifndef UUID_2E86E0D0_CBCB_4862_B999_2DCA528F08E5
+#define UUID_2E86E0D0_CBCB_4862_B999_2DCA528F08E5
+
+#include <string>
+
+namespace path {
+	std::string const root = "assets/";
+	std::string const map = root + "map.jpg";
+	std::string const font = root + "AmazDooMLeft.ttf";
+	std::string const character = root + "character.jpg";
+}
+
+#endif
