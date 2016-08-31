708fdc780c4e946d36d95a041863b86fd591d1d3
SneManden <casper@snemanden.com>
2016-08-31 09:38:06 +0200

    Starting the framework

---
+++ gs_map.hpp
@@ -0,0 +1,17 @@
+#include "game_state.hpp"
+
+class GameStateMap : public GameState {
+	private:
+	
+	sf::View view;
+
+	// ..
+	
+	public:
+	
+	virtual void draw(const float dt);
+	virtual void update(const float dt);
+	virtual void handleInput();
+
+	GameStateMap(Game *game);
+};
