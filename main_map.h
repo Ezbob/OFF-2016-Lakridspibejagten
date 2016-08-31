90a18b4e805a0522b8e7b2d1120f0fbb5ec6819a
Frederik Hertzum <frederik.hertzum@gmail.com>
2016-08-31 09:28:22 +0200

    MainMap -- useless klasse so far

---
+++ main_map.h
@@ -0,0 +1,11 @@
+#ifndef UUID_8DB4C94D_C9A5_45BF_BDBA_FA8F202A4938
+#define UUID_8DB4C94D_C9A5_45BF_BDBA_FA8F202A4938
+
+#include <SFML/Texture/Texture.hpp>
+
+class MainMap : public sf::Texture {
+public:
+	MainMap();
+};
+
+#endif
