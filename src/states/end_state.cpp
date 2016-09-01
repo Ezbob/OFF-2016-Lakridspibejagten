c8e31b83587ae395406bbe8d65daab6883897708
Frederik Hertzum <frederik.hertzum@gmail.com>
2016-09-01 15:14:28 +0200

    fiks merge

--- src/states/end_state.cpp
+++ src/states/end_state.cpp
@@ -18,7 +18,7 @@ void end_state::draw(float const dt) {
 
 	t.setString("Spillet er slut!\n");
 	t.setString(t.getString() + "Piber: " + to_string(game->score_pibe) + "\n");
-	t.setString(t.getString() + "Gaver: " + to_string(game->score_gaver)+ "\n");
+	t.setString(t.getString() + "Gaver: " + to_string(game->score_gave)+ "\n");
 	t.setString(t.getString() + "Glade unger: " + to_string(glade_unger));	
 	t.setFont(assets::font_main);
 
